048b741dea6b094e0fa7c6b4be05f9eaaa92b572
Demonept <66496493+Demonept@users.noreply.github.com>
2022-12-12 13:05:16 +0300

    Создан бин cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanS =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catFir = (Cat) applicationContext.getBean("cat");
+        System.out.println(catFir.getVoice());
+        Cat catSec = (Cat) applicationContext.getBean("cat");
+        System.out.println(catFir==catSec);
+        System.out.println(bean==beanS);
+
         System.out.println(bean.getMessage());
     }
 }
